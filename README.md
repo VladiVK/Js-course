90ba682d5cb89873730e7bbe6571ba3276bac132
VLadiVK <kozobon@gmail.com>
2019-05-04 18:47:14 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#homework 1
+## Budget aplication
---
+++ script.js
@@ -0,0 +1,20 @@
+// Budgete application
+
+let money = +prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let appData = {
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+appData.budgete = money;
+appData.timeDate = time;
+let expenseOne = prompt(
+    "Введите обязательную статью расходов в этом месяце", ""
+);
+appData.expenses[expenseOne] = +prompt("Во сколько обойдется");
+console.log(
+    "Your one day budgete is: " +
+    (appData.budgete - appData.expenses[expenseOne]) / 30
+);
