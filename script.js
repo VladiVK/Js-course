ac1afc2b2fc0cc382faa47994dac35263c2f7adc
VLadiVK <kozobon@gmail.com>
2019-05-05 22:39:35 +0300

    #4 logic was improved

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 // Budgete application. preliminary
 
-let money = +prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 let appData = {
+    budget: money,
     expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
-appData.budgete = money;
-appData.timeDate = time;
-let expenseOne = prompt(
-    "Введите обязательную статью расходов в этом месяце", ""
-);
-appData.expenses[expenseOne] = +prompt("Во сколько обойдется");
-console.log(
-    "Your one day budgete is: " +
-    (appData.budgete - appData.expenses[expenseOne]) / 30
-);
+
+for (let i = 0; i < 1; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = +prompt("Во сколько обойдется");
+    // если в prompt нажать отмана, то получим null
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null &&
+        a != '' && b != '' && a.length <= 50) {
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+
+}
+appData.moneyPerDay = appData.budget / 30;
+
+console.log("Your one day budgete is: " + (appData.moneyPerDay));
+
+if (appData.moneyPerDay < 100) {
+    console.log('minimum level of rich');
+} else if (appData.moneyPerDay < 500) {
+    console.log('middle level of rich');
+} else {
+    console.log('high level of rich');
+}
+
+/* switch (true) {
+    case appData.moneyPerDay < 100:
+        console.log('minimum level of rich');
+        break;
+    case appData.moneyPerDay < 500:
+        console.log('middle level of rich');
+        break;
+    case appData.moneyPerDay > 500:
+        console.log('high level of rich');
+        break;
+    default:
+        console.log('uknown info');
+        break;
+} */
